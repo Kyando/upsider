4c461d7633a6de218e91a34ff25590f7bd9a9b82
Bruno Ribeiro <Brunoribeiro47@hotmail.com>
2024-09-18 21:05:32 -0300

    Fixed inverted gravity to boxes

--- scenes/box_test.gd
+++ scenes/box_test.gd
@@ -4,57 +4,76 @@ const GRAVITY := 400
 
 var should_apply_force := false
 var y_force := 400
-var gravity_scale := -1
+var gravity_scale := 1
 
 var peak_pos = position.y
 var portal_pos_y := 150.0
 var target_peak_pos = null
 
 func _process(delta: float) -> void:
-	if position.y < peak_pos:
-		peak_pos = position.y
+	if gravity_scale == 1:
+		if position.y < peak_pos:
+			peak_pos = position.y
+	elif gravity_scale == -1:
+		if peak_pos < position.y :
+			peak_pos = position.y
 	
 		
 func _physics_process(delta):
-	#if Input.is_action_just_pressed("ui_jump") and is_on_floor():
 	if should_apply_force:
 		
 		var portal_offset = abs(position.y- portal_pos_y)
-		print("peak ", peak_pos)
-		print("y_pos ",position.y)
-		
+		var distance = abs(peak_pos - portal_pos_y)
+		print("----")
+		print("y_peak ", peak_pos)
+		print("y_pos ", position.y)
 		print("y_portal ", portal_pos_y)
-		print("y_pos ",position.y)
-		print("offset ",portal_offset)
-		
-		var distance = abs(peak_pos - position.y)
-		print("peak_distance ", distance)
+		print("offset ", portal_offset)
+		print("dist1 ", distance)
+		if gravity_scale == -1:
+			distance = abs(portal_pos_y - peak_pos)
+		print("dist2 ", distance)
 		distance += portal_offset
-		print("peak_distance + offset ", distance )
+		print("dist3 ", distance)
+	
 		y_force = calculate_portal_force(distance)
-		print("calculated_y_force ", y_force)
-		target_peak_pos = peak_pos
+		#y_force = 200 
+		print("y_force ", y_force)
+		
+		var p_y = position.y
+		target_peak_pos = position.y - (distance * gravity_scale)
+		print("peak_pos ", peak_pos)
+		print("target_pos ", target_peak_pos)
 		peak_pos = position.y
 		
 		
-		velocity.y = y_force * gravity_scale
+		if gravity_scale == -1:
+			velocity.y = y_force
+		else:
+			velocity.y = -y_force
+		print("y_vel ", velocity.y)
 		should_apply_force = false
 	
 	# Apply gravity to the velocity
-	velocity.y += GRAVITY * delta
-	if target_peak_pos and (abs(position.y) - abs(target_peak_pos)) < 1:
-	#if velocity.y < 10 and velocity.y > -10:
-		#print("Stoping on peak")
-		#print("vel ", velocity.y)
-		#print("pos ", position.y)
-		position.y = int(target_peak_pos)
-		velocity.y = 0
-		target_peak_pos = null
+	velocity.y += GRAVITY  * gravity_scale * delta
+	
+	
+	if target_peak_pos: 
+		var target_pos_dist = abs(abs(position.y) - abs(target_peak_pos))
+		print("tartget_pos_dist ", target_pos_dist)
+		if target_pos_dist < 3:
+			print("Whaaat")
+			var valuezinho = abs(abs(position.y) - abs(target_peak_pos))
+			print("valuz: ",valuezinho)
+			position.y = int(target_peak_pos)
+			velocity.y = 0
+			target_peak_pos = null
+		
 	move_and_slide()
 
 func invert_gravity(base_y_pos: float) -> void:
 	#up_direction *= -1
-	#gravity_scale *= -1
+	gravity_scale *= -1
 	portal_pos_y = base_y_pos
 	should_apply_force = true
 	
@@ -69,7 +88,6 @@ func calculate_portal_force(distance):
 	#print("Calculated portal Time: ", jump_time)
 	#print("Calculated Portal Velocity: ", portal_velocity)
 	return portal_velocity
-	#
 	
 	
 	
